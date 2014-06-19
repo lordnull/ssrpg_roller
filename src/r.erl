5311d17e65cc9e180c1e8e4c1ef93f501f309a64
Micah Warren <micahw@lordnull.com>
2014-06-19 16:09:37 -0500

    Cleaned up cli UI and made it useful as roller.
    Also, triumph/despair cancel now.

--- src/r.erl
+++ src/r.erl
@@ -4,7 +4,7 @@
 -include("ssrpg_roller_dice.hrl").
 
 -export([roll/1, successes/1, failures/1, advantages/1, threats/1,
-	triumphs/1, despairs/1, pool/1, from_list/1, from_list/2]).
+	triumphs/1, despairs/1, pool/1, from_list/1, from_list/2, to_string/1]).
 
 -record(r, {
 	pool = [],
@@ -60,10 +60,22 @@ threats(R) ->
 	end.
 
 triumphs(R) ->
-	R#r.triumph.
+	Count = R#r.triumph - R#r.despair,
+	if
+		Count < 0 ->
+			0;
+		true ->
+			Count
+	end.
 
 despairs(R) ->
-	R#r.despair.
+	Count = R#r.despair - R#r.triumph,
+	if
+		Count < 0 ->
+			0;
+		true ->
+			Count
+	end.
 
 from_list(List) ->
 	from_list(undefined, List).
@@ -71,6 +83,23 @@ from_list(List) ->
 from_list(Pool, List) ->
 	R = #r{pool = Pool},
 	lists:foldl(fun res_acc/2, R, List).
+
+to_string(Rec) ->
+	AccessOrder = [despairs, threats, failures, successes, advantages, triumphs],
+	lists:foldl(fun(Prop, Acc) ->
+		Var = case Prop of
+			despairs -> $D;
+			threats -> $t;
+			failures -> $-;
+			successes -> $+;
+			advantages -> $a;
+			triumphs -> $T
+		end,
+		Count = ?MODULE:Prop(Rec),
+		lists:foldl(fun(_, InnerAcc) ->
+			[Var | InnerAcc]
+		end, Acc, lists:seq(1, Count))
+	end, [], AccessOrder).
 
 % =====
 % internal
--- src/ssrpg_roller.erl
+++ src/ssrpg_roller.erl
@@ -3,9 +3,9 @@
 -include("ssrpg_roller_dice.hrl").
 
 -define(opt_specs, [
-	{skill_pool, $r, "skill", {string, [?ability_die]}, "The skill dice being rolled"},
-	{monty, $m, "monty", undefined, "Use monty carlo rather than generated roll chances"},
-	{iterations, $i, "iterations", {integer, 1000000}, "How many times to do the roll when using monty"},
+	{monty, $m, "monty", {integer, undefined}, "Use monty carlo rather than generated roll chances, even if the roll string is short. Overrides -c and -o"},
+	{calc, $c, "calc", undefined, "Use explicite generated roll chances even if hte roll string is long. Overrides -m and -o"},
+	{odds, $o, "odds", undefined, "Generate the odds of various roll results instead of just the results of one roll. This will automatically choose to use calc or monty depending on the length of the roll string."},
 	{help, $?, "help", undefined, "Show a usage thing"}
 ]).
 
@@ -13,67 +13,161 @@
 
 main(Opts) ->
 	random:seed(now()),
-	{ok, {OpList, _Tail} = ParsedOpts} = getopt:parse(?opt_specs, Opts),
+	{ok, {RawOpList, RawRoll}} = getopt:parse(?opt_specs, Opts),
+	Roll = fix_pool(lists:flatten(RawRoll)),
+	OpList = clean_exclusives(RawOpList),
+	maybe_do([
+		fun maybe_help/2,
+		fun maybe_calc/2,
+		fun maybe_monty/2,
+		fun maybe_either/2,
+		fun maybe_roll/2
+	], [OpList, Roll]).
+
+clean_exclusives(RawOpsList) ->
+	Rollmode = lists:foldl(fun
+		(calc, _Mode) ->
+			calc;
+		({monty, undefined}, Mode) ->
+			Mode;
+		({monty, _} = M, _) ->
+			M;
+		(odds, _) ->
+			odds;
+		(_, Mode) ->
+			Mode
+	end, undefined, RawOpsList),
+	FilteredOps = lists:filter(fun
+		(calc) -> false;
+		({monty, _}) -> false;
+		(odds) -> false;
+		(_) -> true
+	end, RawOpsList),
+	[{calc, Rollmode} | FilteredOps].
+
+maybe_do(FunList, Args) ->
+	maybe_do(FunList, false, Args).
+
+maybe_do(_FunList, true, _Args) ->
+	ok;
+
+maybe_do([], _Whatever, _Args) ->
+	help();
+
+maybe_do([Fun | Tail], _, Args) ->
+	Res = erlang:apply(Fun, Args),
+	maybe_do(Tail, Res, Args).
+
+maybe_help(OpList, _) ->
 	case proplists:get_value(help, OpList) of
 		undefined ->
-			Iters = proplists:get_value(iterations, OpList),
-			Pool = fix_pool(proplists:get_value(skill_pool, OpList)),
-			%Soak = proplists:get_value(soak, OpList),
-			%WeaponDmg = proplists:get_value(weapon_damage, OpList),
-			FullResList = case proplists:get_value(monty, OpList) of
-				undefined ->
-					lists:map(fun(R) ->
-						r:from_list(Pool, R)
-					end, g:all_possible(Pool));
-				_ ->
-					lists:map(fun(_) ->
-						RollRes = r:roll(Pool),
-						%Damage = calc_damage(RollRes, WeaponDmg, Soak),
-						RollRes
-					end, lists:seq(1, Iters))
-			end,
-			Props = [
-				{"Success %", m:success_percent(FullResList)},
-				{"Success Mean", m:success_mean(FullResList)},
-				{"Success 50%", m:success_percentile(FullResList, 50)},
-				{"Success 80%", m:success_percentile(FullResList, 80)},
-				{"Success max", m:success_percentile(FullResList, 100)},
-				{"Fail Mean", m:fail_mean(FullResList)},
-				{"Fail 50%", m:fail_percentile(FullResList, 50)},
-				{"Fail 80%", m:fail_percentile(FullResList, 80)},
-				{"Fail max", m:fail_percentile(FullResList, 100)},
-				{"Threat %", m:threat_percent(FullResList)},
-				{"Threat Mean", m:threat_mean(FullResList)},
-				{"Threat 50%", m:threat_percentile(FullResList, 50)},
-				{"Threat 80%", m:threat_percentile(FullResList, 80)},
-				{"Threat max", m:threat_percentile(FullResList, 100)},
-				{"Advantage %", m:advantage_percent(FullResList)},
-				{"Advantage Mean", m:advantage_mean(FullResList)},
-				{"Advantage 50%", m:advantage_percentile(FullResList, 50)},
-				{"Advantage 80%", m:advantage_percentile(FullResList, 80)},
-				{"Advantage max", m:advantage_percentile(FullResList, 100)},
-				{"Triumph %", m:triumph_percent(FullResList)},
-				{"Despair %", m:despair_percent(FullResList)}
-			],
-			lists:foreach(fun({K, V}) ->
-				io:format("~s: ~p~n", [K, V])
-			end, Props);
+			false;
 		_ ->
-			getopt:usage(?opt_specs, "ssrpg_roller"),
-			io:format(""
+			help(),
+			true
+	end.
+
+maybe_calc(OpList, Roll) ->
+	case proplists:get_value(calc, OpList) of
+		calc ->
+			calc(Roll),
+			true;
+		_ ->
+			false
+	end.
+
+maybe_monty(OpList, Roll) ->
+	case proplists:get_value(calc, OpList) of
+		{monty, N} ->
+			monty(Roll, N),
+			true;
+		_ ->
+			false
+	end.
+
+maybe_either(OpList, Roll) ->
+	case proplists:get_value(calc, OpList) of
+		odds ->
+			if
+				length(Roll) >= 6 ->
+					monty(Roll, 1000000);
+				true ->
+					calc(Roll)
+			end,
+			true;
+		_ ->
+			false
+	end.
+
+maybe_roll(OpList, Roll) ->
+	case proplists:get_value(calc, OpList) of
+		undefined ->
+			roll(Roll),
+			true;
+		_ ->
+			false
+	end.
+
+monty(Roll, Iters) ->
+	FullRes = lists:map(fun(_) ->
+		r:roll(Roll)
+	end, lists:seq(1, Iters)),
+	output_odds(FullRes).
+
+calc(Roll) ->
+	FullRes = lists:map(fun(R) ->
+		r:from_list(Roll, R)
+	end, g:all_possible(Roll)),
+	output_odds(FullRes).
+
+roll(Roll) ->
+	Res = r:roll(Roll),
+	Str = Res:to_string(),
+	io:format("~s~n", [Str]).
+
+output_odds(FullResList) ->
+	Props = [
+		{"Success %", m:success_percent(FullResList)},
+		{"Success Mean", m:success_mean(FullResList)},
+		{"Success 50%", m:success_percentile(FullResList, 50)},
+		{"Success 80%", m:success_percentile(FullResList, 80)},
+		{"Success max", m:success_percentile(FullResList, 100)},
+		{"Fail Mean", m:fail_mean(FullResList)},
+		{"Fail 50%", m:fail_percentile(FullResList, 50)},
+		{"Fail 80%", m:fail_percentile(FullResList, 80)},
+		{"Fail max", m:fail_percentile(FullResList, 100)},
+		{"Threat %", m:threat_percent(FullResList)},
+		{"Threat Mean", m:threat_mean(FullResList)},
+		{"Threat 50%", m:threat_percentile(FullResList, 50)},
+		{"Threat 80%", m:threat_percentile(FullResList, 80)},
+		{"Threat max", m:threat_percentile(FullResList, 100)},
+		{"Advantage %", m:advantage_percent(FullResList)},
+		{"Advantage Mean", m:advantage_mean(FullResList)},
+		{"Advantage 50%", m:advantage_percentile(FullResList, 50)},
+		{"Advantage 80%", m:advantage_percentile(FullResList, 80)},
+		{"Advantage max", m:advantage_percentile(FullResList, 100)},
+		{"Triumph %", m:triumph_percent(FullResList)},
+		{"Despair %", m:despair_percent(FullResList)}
+	],
+	lists:foreach(fun({K, V}) ->
+		io:format("~s: ~p~n", [K, V])
+	end, Props).
+
+help() ->
+	getopt:usage(?opt_specs, "ssrpg_roller"),
+	io:format(""
 "Dice are represented using the first letter of the name of the die. You~n"
 "can also use the color of the die if you choose to. The colors are~n"
 "represented by a capital letter:~n"
-				"    U: boost (blue)~n"
-				"    G: ability (green)~n"
-				"    Y: proficiency (yellow)~n"
-				"    B: setback (black)~n"
-				"    P: difficulty (purple)~n"
-				"    R: challenge (red)~n"
-				"~n"
+		"    U: boost (blue)~n"
+		"    G: ability (green)~n"
+		"    Y: proficiency (yellow)~n"
+		"    B: setback (black)~n"
+		"    P: difficulty (purple)~n"
+		"    R: challenge (red)~n"
+		"~n"
 "So YGGPRB and paadcs is 1 profciency, 2 ability, 1 difficulty, 1 challenge,"
-"and 1 setback die.~n")
-	end.
+"and 1 setback die.~n").
 
 fix_pool(In) ->
 	lists:map(fun
